24e444578452ce9b89f893b075df785b2bcd454d
95-Fernando <ferpoot95@gmail.com>
2023-12-09 14:46:28 -0600

    Shopping Counter 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import libroDeCocina from "./assets/libro-de-cocina.jpg"
+import React, { useState } from 'react';
 
 function App() {
+
+/* useState, valor inicial 0 y función setCounter para modificar el valor inicial*/ 
+  const [counter, setCounter] = useState(0);
+
+  /* Función para incrementar el contador*/ 
+  const clickPlus = () => {
+    setCounter(counter + 1);
+  };
+
+  /* Función para disminuir el contador*/
+  const clickMinus = () => {
+    if(counter > 0 ) {
+      setCounter( counter -1)
+    }
+  }
+  /* Función para simular la compra, solo recarga la página*/
+  const buy = () => {
+    window.location.reload();
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <div className = "main__container">
+        <h1>Shopping Counter</h1>
+        <div className="image__container">
+          <img src= {libroDeCocina} alt="libro de cocina"/>
+        </div>
+        <p>El Gran Libro de la Cocina Tradicional</p>
+        <p>Precio: <a>$499.00</a></p>
+        <div className = "pieces__container">
+          <button className = "button__minus" onClick = {clickMinus}>--</button>
+          <a className = "pieces">{counter} Piezas </a>
+          <button className = "button__plus" onClick = {clickPlus}>+</button>
+        </div>
+        <button className="button__buy" onClick = {buy} >Comprar</button>
+      </div>
     </div>
   );
 }
